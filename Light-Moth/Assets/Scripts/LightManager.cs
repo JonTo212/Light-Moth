8f0829c12b133785ec3d2c91338479c57b6caa6b
Jonathan To <jonto398@gmail.com>
2023-05-13 21:18:45 -0400

    Added center point functionality
    Intensity currently not working

--- Light-Moth/Assets/Scripts/LightManager.cs
+++ Light-Moth/Assets/Scripts/LightManager.cs
@@ -1,40 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LightManager : MonoBehaviour
 {
     GameObject[] lightObjects;
-    GameObject[] usableLights;
+    List<GameObject> usableLights;
     List<Light> lights;
     Moth moth;
+    float distance;
 
     void Start()
     {
         moth = GameObject.FindGameObjectWithTag("Player").GetComponent<Moth>();
         lights = new List<Light>();
         lightObjects = GameObject.FindGameObjectsWithTag("Light");
+        usableLights = new List<GameObject>();
         foreach (GameObject go in lightObjects)
         {
-            if (go.GetComponent<Light>() != null)
+            if (go.GetComponent<Light>() != null && (Vector3.Distance(go.transform.position, moth.transform.position) <= moth.radius))
             {
                 lights.Add(go.GetComponent<Light>());
-            }
-        }
-    }
-
-    void Update()
-    {
-        UsableLights();
-    }
-
-    void UsableLights()
-    {
-        foreach (Light light in lights)
-        {
-            if (Vector3.Distance(light.transform.position, moth.transform.position) <= moth.radius)
-            {
-                //light.OnOffOpen();
+                usableLights.Add(go);
             }
         }
     }
--- Light-Moth/Assets/Scripts/Moth.cs
+++ Light-Moth/Assets/Scripts/Moth.cs
@@ -6,15 +6,20 @@ public class Moth : MonoBehaviour
 {
     public float speed;
     public float radius;
+    Light[] activeLights;
+    float[] activeLightIntensities;
 
     void FixedUpdate()
     {
-        MoveTowards();
+        GetLights();
     }
 
-    void MoveTowards()
+    void GetLights()
     {
         Collider[] lights = Physics.OverlapSphere(transform.position, radius, LayerMask.GetMask("Light"));
+        activeLights = new Light[lights.Length];
+        activeLightIntensities = new float[lights.Length];
+        Vector3 centerPoint = Vector3.zero;
 
         if (lights.Length > 0)
         {
@@ -24,12 +29,36 @@ public class Moth : MonoBehaviour
 
                 if (light.isOn)
                 {
-                    Vector3 vectorTowardsLight = lights[i].transform.position - transform.position;
-
-                    transform.position = Vector3.MoveTowards(transform.position, lights[i].transform.position, speed * light.intensity * Time.deltaTime);
+                    //MoveTo(lights[i].transform.position, light.intensity);
+                    activeLights[i] = light;
+                    activeLightIntensities[i] = light.intensity;
+                    centerPoint = GetCenterPoint(activeLights);
                 }
             }
         }
+        MoveTo(centerPoint);
+    }
+
+    void MoveTo(Vector3 target)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    Vector3 GetCenterPoint(Light[] lights)
+    {
+        Vector3 center = Vector3.zero;
+        float count = 0;
+        for (int i = 0; i < lights.Length; i++)
+        {
+            if (lights[i] != null)
+            {
+                center += lights[i].transform.position;
+                count++;
+            }
+        }
+        center = center / count;
+        print(center);
+        return center;
     }
 
     void OnDrawGizmos()
